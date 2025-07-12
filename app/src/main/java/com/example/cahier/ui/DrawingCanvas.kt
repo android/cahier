cb8daaf8d241e1df78f83032fffc7d3710f9fdfb
MABD <mabd.universe@gmail.com>
2025-07-12 06:14:48 +0300

    Add missing import

--- app/src/main/java/com/example/cahier/ui/DrawingCanvas.kt
+++ app/src/main/java/com/example/cahier/ui/DrawingCanvas.kt
@@ -40,6 +40,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
