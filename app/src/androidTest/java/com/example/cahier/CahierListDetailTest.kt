5523817167a5683fca03254dbb80c70bbb0ff803
Rob Orgiu <roberto.orgiu@gmail.com>
2025-11-06 14:08:15 +0100

    Remove unused imports

--- app/src/androidTest/java/com/example/cahier/CahierListDetailTest.kt
+++ app/src/androidTest/java/com/example/cahier/CahierListDetailTest.kt
@@ -2,18 +2,14 @@ package com.example.cahier
 
 import androidx.compose.ui.test.DeviceConfigurationOverride
 import androidx.compose.ui.test.ForcedSize
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.cahier.data.FakeNotesRepository
-import com.example.cahier.ui.CahierApp
 import com.example.cahier.ui.HomePane
 import com.example.cahier.ui.viewmodels.HomeScreenViewModel
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
